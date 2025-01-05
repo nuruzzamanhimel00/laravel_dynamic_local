c386dd024ee9847c954553e68068eb5c1cd7c816
nuruzzamanhimel00 <nuruzzaman.himel147@gmail.com>
2025-01-06 00:09:39 +0600

    dynamin lang store

---
+++ app/Models/Post.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
+
+class Post extends Model
+{
+    use HasTranslations;
+
+    public $translatable = ['title','body'];
+    protected $guarded = ['id'];
+}
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,48 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Models\Post;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/{lang?}', function ($lang = 'en') {
+
+    App::setLocale($lang);
+    $post = Post::first();
+    return view('welcome', compact('post','lang'));
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::post('/store', function (Request $request) {
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+    $lang = $request->get('lang') ;
+    App::setLocale($lang);
+    
+    $postData = [
+        'title' => $request->title,
+        'body' => $request->body,
+    ];
+    if(Post::first()){
+        $post = Post::first();
+        $post->title = $request->title;
+        $post->body = $request->body;
+        $post->save();
+    }
+    else{
+        Post::create($postData);
+    }
+    return back();
+})->name('store');
 
-require __DIR__.'/auth.php';
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
+
+// require __DIR__.'/auth.php';
